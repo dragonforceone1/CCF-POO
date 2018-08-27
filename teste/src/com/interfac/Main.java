a80aac386ea61fe4abdaccf4924bc68d9626c4b9
Pedro Mariano <dragonforceone1@users.noreply.github.com>
2018-08-27 15:05:21 -0300

    Add teste intelij

---
+++ teste/src/com/interfac/Main.java
@@ -0,0 +1,10 @@
+package com.interfac;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"teste");
+    }
+}
